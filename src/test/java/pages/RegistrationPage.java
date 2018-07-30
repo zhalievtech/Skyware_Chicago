4f894c22ab4e9dc756fc6eb8ccac71b59a6aee99
Asel Sadykova <aselsadykova61@gmail.com>
2018-07-29 20:40:24 -0500

    box elements added

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import utilities.Driver;
@@ -8,9 +10,41 @@ public class RegistrationPage {
 
 	public RegistrationPage() {
 		PageFactory.initElements(Driver.getDriver(), this);
+		
 	}
 
+	@FindBy(id="accountName")
+	public WebElement accountNameBox;
 	
+	
+@FindBy(id="profile_firstName")
+public WebElement firstNameBox;
+
+@FindBy(id="profile_lastName")
+public WebElement lastNameBox;
+
+@FindBy(id="countrySelect")
+public WebElement countrySelect;
+
+@FindBy(id="profile_address1")
+public WebElement addressBox1;
+
+@FindBy(id="profile_address2")
+public WebElement addressBox2;
+
+@FindBy(id="profile_city")
+public WebElement cityBox;
+
+@FindBy(id="stateSelect")
+public WebElement stateSelect;
+
+@FindBy(id="profile_zip")
+public WebElement zipBox;
+
+@FindBy(id="profile_phone")
+public WebElement phoneBox;
+
+
 
 }
 
