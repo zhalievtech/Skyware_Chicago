a8c72ecf75021aee8582252eca68987c672f2e78
hasanistan <hasanistan1@gmail.com>
2018-07-29 20:47:22 -0500

    registration page elements added

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import utilities.Driver;
@@ -10,7 +12,31 @@ public class RegistrationPage {
 		PageFactory.initElements(Driver.getDriver(), this);
 	}
 
+	@FindBy(xpath="//div[@class='profile-info']/label[1]")
+	public WebElement companyNameText;
 	
+	@FindBy(xpath="//div[@class='profile-info']/label[2]")
+	public WebElement firstNameText;
+	
+	@FindBy(xpath="//div[@class='profile-info']/label[3]")
+	public WebElement lastNameText;
+	
+	@FindBy(xpath="//div[@class='profile-info']/label[4]")
+	public WebElement countryText;
+	
+	@FindBy(xpath="//div[@class='profile-info']/label[7]")
+	public WebElement cityText;
+	
+	@FindBy(xpath="//div[@class='profile-info']/label[8]")
+	public WebElement stateText;
+	
+	@FindBy(id="licenseAccepted")
+	public WebElement accepteCheckbox;
+	
+	@FindBy(xpath="//div[@class='license-info']/span")
+	public WebElement checkBoxStar;
+	
+	//
 
 }
 
